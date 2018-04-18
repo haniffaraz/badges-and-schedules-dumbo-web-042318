1c7bb46bf1b2813a8aeae2a2f99d1dbc216e0113
Faraz Hanif <hfaraz745@gmail.com>
2018-04-18 02:20:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end  
