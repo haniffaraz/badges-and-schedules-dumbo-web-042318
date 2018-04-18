c1440895826129a91052fb7b73be47a8caa77697
Faraz Hanif <hfaraz745@gmail.com>
2018-04-18 02:29:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,11 @@ def batch_badge_creator(attendees)
     "Hello, my name is #{name}."
   end
 end
+
+def assign_rooms(attendees)
+  counter = 0
+  attendees.collect do |name|
+      counter += 1
+      "Hello, #{name}! You'll be assigned to room #{counter}!"
+  end
+end
